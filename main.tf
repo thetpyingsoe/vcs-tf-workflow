36a8be639218758a5940ccfe69f9912759651988
Thet <mgthetpinesoe@gmail.com>
2025-02-15 16:26:31 +0800

    Create Second VPC

--- main.tf
+++ main.tf
@@ -5,3 +5,11 @@ resource "aws_vpc" "hello_vpc" {
   enable_dns_support   = true
 
 }
+
+resource "aws_vpc" "hello_vpc-2" {
+
+  cidr_block           = "192.168.0.0/24"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+}
