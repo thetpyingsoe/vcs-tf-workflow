5d9d29c867017f05df82d3e1d936e3c136d0603b
Thet <mgthetpinesoe@gmail.com>
2025-02-15 16:30:24 +0800

    Delete VPC

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
-resource "aws_vpc" "hello_vpc" {
+# resource "aws_vpc" "hello_vpc" {
 
-  cidr_block           = "192.168.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
+#   cidr_block           = "192.168.0.0/16"
+#   enable_dns_hostnames = true
+#   enable_dns_support   = true
 
-}
+# }
 
-resource "aws_vpc" "hello_vpc-2" {
+# resource "aws_vpc" "hello_vpc-2" {
 
-  cidr_block           = "192.168.0.0/24"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
+#   cidr_block           = "192.168.0.0/24"
+#   enable_dns_hostnames = true
+#   enable_dns_support   = true
 
-}
+# }
